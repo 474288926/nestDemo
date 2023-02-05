6859a95c105a8d297442f40776af75269983dcd2
yang <474288926@qq.com>
2023-02-05 13:51:17 +0800

    delete:删除api-key.guard

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { ApiKeyGuard } from './guards/api-key.guard';
 
 @Module({
   imports: [ConfigModule],
